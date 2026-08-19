357332a9fdc9b8a1dea49b9941169acacfa68b34
Alejandro Jose <76195052+akelaonline@users.noreply.github.com>
2026-08-19 10:01:16 -0300

    docs: preserve approved Akela author branding in alpha.7

--- wp-nerve.php
+++ wp-nerve.php
@@ -7,8 +7,8 @@
  * Version:           0.1.0-alpha.7
  * Requires at least: 6.9
  * Requires PHP:      8.1
- * Author:            Akela (@akelaonline)
- * Author URI:        https://www.instagram.com/akelaonline/
+ * Author:            Alejandro Daniel José · Akela
+ * Author URI:        https://mktmarketingdigital.com
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       wp-nerve
