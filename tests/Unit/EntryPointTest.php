bdfb30dd3cdf2de3fcc09dc889e777dbbde99c4b
Alejandro Jose <76195052+akelaonline@users.noreply.github.com>
2026-08-19 10:20:54 -0300

    test: expect alpha.8 runtime version

--- tests/Unit/EntryPointTest.php
+++ tests/Unit/EntryPointTest.php
@@ -46,7 +46,7 @@ final class EntryPointTest extends PHPUnitTestCase
 
     public function testLoadingEntryPointBootsThePlugin(): void
     {
-        self::assertSame('0.1.0-alpha.7', WP_NERVE_VERSION);
+        self::assertSame('0.1.0-alpha.8', WP_NERVE_VERSION);
 
         self::assertCount(1, WPState::$activationHooks);
         self::assertSame(array(Activator::class, 'activate'), WPState::$activationHooks[0]['callback']);
