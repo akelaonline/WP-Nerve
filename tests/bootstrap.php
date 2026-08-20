e96416d7a7c36698b67c7b20c3c635bee07ffaeb
Alejandro Jose <76195052+akelaonline@users.noreply.github.com>
2026-08-19 23:25:51 -0300

    test: align bootstrap version with alpha.11

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -28,7 +28,7 @@ require_once $fixtures . '/wp-filesystem-functions.php';
 defined('ABSPATH') || define('ABSPATH', $fixtures . '/');
 defined('WP_CONTENT_DIR') || define('WP_CONTENT_DIR', dirname(__DIR__) . '/wp-content');
 defined('WP_PLUGIN_DIR') || define('WP_PLUGIN_DIR', WP_CONTENT_DIR . '/plugins');
-defined('WP_NERVE_VERSION') || define('WP_NERVE_VERSION', '0.1.0-alpha.10');
+defined('WP_NERVE_VERSION') || define('WP_NERVE_VERSION', '0.1.0-alpha.11');
 defined('WP_NERVE_FILE') || define('WP_NERVE_FILE', dirname(__DIR__) . '/wp-nerve.php');
 defined('WP_NERVE_PATH') || define('WP_NERVE_PATH', dirname(__DIR__) . '/');
 defined('WP_NERVE_URL') || define('WP_NERVE_URL', 'https://example.test/wp-content/plugins/wp-nerve/');
